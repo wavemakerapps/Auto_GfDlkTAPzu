f1a04a5b11e71dc6e18878c31f82700a39f1f365
root <root@61949b3d2f95>
2017-07-19 18:03:25 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_GfDlkTAPzu",
+  "homePage" : "Main",
+  "name" : "Auto_GfDlkTAPzu",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
